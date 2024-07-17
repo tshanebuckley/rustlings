80548e5b68a78390ac39cff05e813188d3ef99cc
Shane Buckley <tshanebuckley@gmail.com>
2024-07-16 20:24:48 -0400

    finished rustlings

---
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -0,0 +1,30 @@
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
+
+    vec.push(88);
+
+    vec
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // TODO: Make both vectors `vec0` and `vec1` accessible at the same time to
+    // fix the compiler error in the test.
+    #[test]
+    fn move_semantics2() {
+        let vec0 = vec![22, 44, 66];
+
+        let vec_clone = vec0.clone();
+
+        let vec1 = fill_vec(vec_clone);
+
+        assert_eq!(vec0, [22, 44, 66]);
+        assert_eq!(vec1, [22, 44, 66, 88]);
+    }
+}
---
+++ exercises/08_enums/enums2.rs
@@ -0,0 +1,28 @@
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Message {
+    // TODO: Define the different variants used below.
+    Move { x: u32, y: u32},
+    Echo(String),
+    ChangeColor(u32, u32, u32),
+    Quit,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{self:?}");
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Move { x: 10, y: 30 },
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
